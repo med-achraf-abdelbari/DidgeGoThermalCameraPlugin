ecfed702b5c6898d6dd9e6485c5e4b6c490cbd77
Mohamed Achref Abdelbari <med.achraf.abdelbari@gmail.com>
2023-03-30 00:46:37 +0100

    FIX

--- www/DidgeGoThermalPlugin.js
+++ www/DidgeGoThermalPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'DidgeGoThermalPlugin', 'coolMethod', [arg0]);
+exports.startThermalCamera = function (arg0, success, error) {
+    exec(success, error, 'DidgeGoThermalPlugin', 'openNewActivity', [arg0]);
 };
