d27a02b6c48659ad7bac9058463bc056adf05d5b
Mohamed Achref Abdelbari <med.achraf.abdelbari@gmail.com>
2023-03-30 00:27:18 +0100

    FIX

--- src/android/DidgeGoThermalPlugin.java
+++ src/android/DidgeGoThermalPlugin.java
@@ -12,38 +12,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 /**
  * This class echoes a string called from JavaScript.
  */
 public class DidgeGoThermalPlugin extends CordovaPlugin {
 
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-    }
+   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+           super.initialize(cordova, webView);
+       }
 
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Context context = cordova.getActivity().getApplicationContext();
-        if (action.equals("ThermalCameraActivity")) {
-            String message = args.getString(0);
-            this.openNewActivity(context);
-            return true;
-        }
-        return false;
-    }
+       @Override
+       public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+           Context context = cordova.getActivity().getApplicationContext();
+           if (action.equals("ThermalCameraActivity")) {
+               String message = args.getString(0);
+               this.openNewActivity(context);
+               return true;
+           }
+           return false;
+       }
 
-    private void openNewActivity(Context context) {
-            Intent intent = new Intent(context, mfc.ThermalCameraActivity.class);
-            this.cordova.getActivity().startActivity(intent);
-    }
-
-//     private void coolMethod(String message, CallbackContext callbackContext) {
-//         if (message != null && message.length() > 0) {
-//             callbackContext.success(message);
-//         } else {
-//             callbackContext.error("Expected one non-empty string argument.");
-//         }
-//     }
+       private void openNewActivity(Context context) {
+               Intent intent = new Intent(context, ThermalCameraActivity.class);
+               this.cordova.getActivity().startActivity(intent);
+       }
 }
