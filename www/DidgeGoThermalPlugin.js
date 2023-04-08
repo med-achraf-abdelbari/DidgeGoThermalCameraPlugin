f501fa133dd1e1c1660684ce0af6452cfdb5df1a
Mohamed Achref Abdelbari <med.achraf.abdelbari@gmail.com>
2023-04-09 00:20:34 +0100

    [UPDATE] FIX ACTIVITY IN MANIFEST

--- www/DidgeGoThermalPlugin.js
+++ www/DidgeGoThermalPlugin.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.startThermalCamera = function (arg0, success, error) {
-    exec(success, error, 'DidgeGoThermalPlugin', 'ThermalCameraActivity', [arg0]);
+    exec(success, error, 'DidgeGoThermalPlugin', 'openThermalCamera', [arg0]);
 };
 
 exports.closeThermalCamera = function (arg0, success, error) {
-    exec(success, error, 'DidgeGoThermalPlugin', 'CloseThermalCameraActivity', [arg0]);
+    exec(success, error, 'DidgeGoThermalPlugin', 'CloseThermalCamera', [arg0]);
 };
